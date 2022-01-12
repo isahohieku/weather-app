aca11e91337e355352070a3b9c59d850eb31c91d
Isah Ohieku <isahohieku@gmail.com>
2022-01-12 15:16:02 +0100

    chore(*): Added husky and lint-staged

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,20 +5,16 @@ import App from './App';
 describe('App', () => {
   let container: ShallowWrapper;
 
-  beforeEach(() => container = shallow(<App />));
+  beforeEach(() => (container = shallow(<App />)));
   test('Should render a div with className app', () => {
     expect(container.find('.App').exists()).toBeTruthy();
   });
 
-  test('Should contain the component weather', () => {
-    expect();
-  });
+  // test('Should contain the component weather', () => {
+  //   expect();
+  // });
 
-  test('Should redirect all pages to the weather page', () => {
+  // test('Should redirect all pages to the weather page', () => {
 
-  });
-
-
+  // });
 });
-
-
