bd3ea3e814ee4dbec4c1747de162930b07e982ee
Isah Ohieku <isahohieku@gmail.com>
2022-01-12 08:32:33 +0100

    chore(*): Added atomic design folders

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import type { ShallowWrapper } from 'enzyme';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  let container: ShallowWrapper;
+
+  beforeEach(() => container = shallow(<App />));
+  test('Should render a div with className app', () => {
+    expect(container.find('.App').exists()).toBeTruthy();
+  });
+
+  test('Should contain the component weather', () => {
+    expect();
+  });
+
+  test('Should redirect all pages to the weather page', () => {
+
+  });
+
+
 });
+
+
---
+++ src/organisms/README.md
@@ -0,0 +1,3 @@
+### Organisms
+
+This folder contains all basic combinations of two or more molecules of the application
